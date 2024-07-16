17f9e85e492c3cb1a0059cfc2bef78abac05603d
dim <2080208310@qq.com>
2024-07-16 19:15:15 +0800

    testproclock

--- user/testprocnum.c
+++ user/testprocnum.c
@@ -1,10 +1,46 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-int
-main(void)
-{
-  int num = getprocnum();
-  printf("Number of processes: %d\n", num);
-  exit(0);
+struct spinlock {
+    uint locked;
+};
+
+void initlock(struct spinlock *lk) {
+    lk->locked = 0;
+}
+
+void acquire(struct spinlock *lk) {
+    while(__sync_lock_test_and_set(&lk->locked, 1) != 0)
+        ; 
+}
+
+void release(struct spinlock *lk) {
+    __sync_lock_release(&lk->locked);
+}
+
+struct spinlock proclock;
+
+int main(void) {
+    initlock(&proclock);
+
+    acquire(&proclock);
+    int num = getprocnum();
+        printf("Number of processes: %d\n", num);
+    int pid = fork();
+    if (pid < 0) {
+        printf("Fork failed\n");
+        release(&proclock);
+        exit(1);
+    } else if (pid == 0) {
+        // kid proc
+        num = getprocnum();
+        printf("Number of processes: %d\n", num);
+        release(&proclock);
+        exit(0);
+    } else {
+        // parent proc
+        wait(0);
+        release(&proclock);
+    }
+    exit(0);
 }
